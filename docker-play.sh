50726e0c0fe48df2dad412d907542909f03a5764
ChristianDaniel <ruizdiazchristiand@gmail.com>
2016-05-16 20:03:11 -0300

    Agregado parametro al script de docker-play para indicar el nombre de la imagen de mongo a la que se linkea

--- docker-play.sh
+++ docker-play.sh
@@ -1,11 +1,17 @@
+if [[ $1 != "" ]]; then
+	mongoname=$1
+else
+    mongoname="mongoplay"
+fi
+echo $mongoname
 # Scrip para levantar la aplicación en docker con una base mongo
 echo "--Ejecutando script--"
 echo "Trayendo y corriendo imagen de mongo"
 docker pull mongo
-docker run -d --name mongoplay mongo
-docker start mongoplay
+docker run -d --name $mongoname mongo
+docker start $mongoname
 echo "Generando la imagen del proyecto"
 ./activator docker:stage
 ./activator docker:publishLocal
 echo "corriendo el proyecto"
-docker run -t -i -p 9000:9000 --rm --link mongoplay:mongoplay play-scala-2-4:1.0-SNAPSHOT bin/
+docker run -t -i -p 9000:9000 --rm --link $mongoname:$mongoname play-scala-2-4:1.0-SNAPSHOT bin/
