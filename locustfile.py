3b689355cb8f514cd102610ae662f8098d462cc4
tat <tatianamolinari90@gmail.com>
2016-05-16 16:03:02 -0300

    comiteando  test de carga

---
+++ locustfile.py
@@ -0,0 +1,43 @@
+from locust import HttpLocust, TaskSet, task
+
+#Correr con locust -f locustfile.py --host=http://localhost:9000
+
+import random
+
+
+def crear_shop(l):
+    lats = [0.1,0.5,2.9,0.6,2.4,0.7,2.1,3.5,2.2,0.2,3.3,1.2,0.8,1.6,1.8,1.7,2.8,3.8,3.8,2.7,9.8,6.1,7.8,2.1,6.8]
+    longs = [9.1,9.5,9.9,9.6,9.4,9.7,9.81,78.5,142.2,123.1,987.3,14.2,99.8,45.6,13.8,188.7,223.8,324.8,30.8,223.7,989.8,62.1,74.8,256.1,67.8]
+    address = ['Calle falsa 123', 'Suipacha 3666', 'Nosedonde 1234', 'UnLugar 4567', 'Av. Corrientes 1233', 'Guido 1298', 'Pringles 9876', 'Av Bergara 2323']
+    location = ['Manantiales Chicos', 'Mendoza', 'General Alvear', 'Departamento de Cafayate', 'Cafayate','Gobernador Benegas', 'Azcuenaga', 'Olaeta', 'Los Condores', 'De la Garma', 'La Calera', 'Lartigau', 'Arroyo Cabral', 'Gobernador Gregores', 'Santa Regina', 'Amadores', 'Guerrico', 'San Carlos Sur', 'Alto de la Piedra', 'Puerto Ruiz', 'El Pueblito']    
+    name = ['Los chinos', 'Los chinos II', 'Lo de irene', 'Carrefour', 'Coto', 'Walmart', 'Econo', 'Los 12 hermanos']
+
+    l.client.post('/shops',
+        {'latitude': random.choice(lats),
+        'longitude': random.choice(longs),
+        'address':random.choice(address), 
+        'name': random.choice(name),
+        'location': random.choice(location)})
+
+
+def home(l):
+    l.client.get("/")
+
+def shops(l):
+    l.client.get("/shops")
+
+def shopsConOffsetYLimit(l):
+    limit = random.choice([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,150,200])
+    offset = random.choice([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,150,200])
+    l.client.get("/shops?offset="+str(offset)+"&limit="+str(limit))
+
+def found_prices(l):
+    l.client.get("/found-prices")
+
+class UserBehavior(TaskSet):
+    tasks = {crear_shop:8,shops:1,shopsConOffsetYLimit:5,found_prices:2,home:3}
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait=5000
+    max_wait=9000
