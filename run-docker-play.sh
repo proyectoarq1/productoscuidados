740b0bf73ab788e1a9033949e5d513e9283ac02e
tatianamolinari <tatianamolinari90@gmail.com>
2016-06-29 00:03:32 -0300

    agregand cambios en el script para poder correr con varias instancias de mongo

--- run-docker-play.sh
+++ run-docker-play.sh
@@ -2,6 +2,9 @@ answer=""
 memoryLimit="0"
 cpuCores="0"
 cpuProportion="1024"
+swapMemory="-1"
+mongoNodes="1"
+mongoNodesSetted="0"
 #
 if [ "$1" != "" ]; then
 	mongoname=$1
@@ -39,6 +42,20 @@ if [ "$answer" = "Y" ];then
 		cpuProportion="1024"
 	fi
 	echo "C -> $cpuProportion"
+	#
+	echo "Enter Swap Memory: "
+	read swapMemory
+	if [ "$swapMemory" = "" ];then
+		swapMemory="-1"
+	fi
+	echo "Sm -> $swapMemory"
+	#
+	echo "Enter Mongo Nodes 1-3: "
+	read mongoNodes
+	if [ "$mongoNodes" = "" ];then
+		mongoNodes="1"
+	fi
+	echo "Mongo Nodes -> $mongoNodes"
 else
 	echo "* Default values"
 fi
@@ -59,11 +76,95 @@ fi
 #    cpuProportion="1024"
 #fi
 #echo $mongoname
-echo "--Ejecutando script--"
-echo "Trayendo y corriendo imagen de mongo"
-docker pull mongo
-docker run -d --name $mongoname mongo
-docker start $mongoname
+echo "--Executing script--"
+
 
 echo "corriendo el proyecto play...."
-docker run -h "productosCuidadosDocker" -t -i -p 9000:9000 --rm -m $memoryLimit --cpu-shares=$cpuProportion --cpuset-cpus=$cpuCores --link $mongoname:$mongoname play-scala-2-4:1.0-SNAPSHOT bin/
+
+if [ "$mongoNodes" = "1" ];then
+	echo "Pulling and running mongo image"
+	docker pull mongo
+
+	echo "Removing existing mongo images"
+	docker rm -f $mongoname
+
+	docker run -d --name $mongoname mongo
+	
+	mongoNodesSetted="1"
+	#Una instancia de mongo
+	echo "Running container with one node of mongo"
+	docker run -h "productosCuidadosDocker" -t -i -p 9000:9000 --rm -m $memoryLimit --memory-swap $swapMemory --cpu-shares=$cpuProportion --cpuset-cpus=$cpuCores --link $mongoname:$mongoname play-scala-2-4:1.0-SNAPSHOT bin/
+
+fi
+
+if [ "$mongoNodes" = "2" ];then
+	echo "Pulling and running two mongo images"
+	docker pull mongo
+
+	echo "Removing existing mongo images"
+	docker rm -f mongo1
+	docker rm -f mongo2
+	
+	docker run -d --expose 27017 --name mongo1 mongo --replSet rsmongo
+	docker run -d --expose 27017 --name mongo2 mongo --replSet rsmongo
+
+	MONGODB1=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongo1)
+	MONGODB2=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongo2)
+
+	echo $MONGODB1
+	echo $MONGODB2
+
+	mongoNodesSetted="1"
+	echo 'Conecting mongo1 with mongo2 in replica set'
+	#docker exec mongo1 mongo --eval 'rs.initiate(); cfg = rs.conf(); cfg.members[0].host = "'$MONGODB1':27017"; rs.reconfig(cfg); rs.add("'$MONGODB2':27017"); rs.status();'
+	docker exec mongo1 mongo --eval "rs.initiate();"
+	sleep 5
+	docker exec mongo1 mongo --eval "cfg = rs.conf(); cfg.members[0].host = '$MONGODB1:27017'; rs.reconfig(cfg);"
+	sleep 5
+	docker exec mongo1 mongo --eval "rs.add('$MONGODB2:27017');"
+	sleep 5
+	docker exec mongo1 mongo --eval "rs.status();"
+
+	#Dos instancias de mongo
+	echo "Running container with two nodes of mongo"
+	docker run -h "productosCuidadosDocker" -t -i -p 9000:9000 --rm -m $memoryLimit --memory-swap $swapMemory --cpu-shares=$cpuProportion --cpuset-cpus=$cpuCores --link mongo1:mongo1 --link mongo2:mongo2 play-scala-2-4:1.0-SNAPSHOT bin/
+
+
+fi
+
+if [ "$mongoNodes" != "2" ] && [ "$mongoNodes" != "1" ] && [ "$mongoNodesSetted" = "0" ];then
+	echo "Pulling and running three mongo images"
+	docker pull mongo
+	
+	echo "Removing existing mongo images"
+	docker rm -f mongo1
+	docker rm -f mongo2
+	docker rm -f mongo3
+	
+	docker run -d --expose 27017 --name mongo1 mongo --replSet rsmongo
+	docker run -d --expose 27017 --name mongo2 mongo --replSet rsmongo
+	docker run -d --expose 27017 --name mongo3 mongo --replSet rsmongo
+
+	MONGODB1=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongo1)
+	MONGODB2=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongo2)
+	MONGODB3=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongo3)
+
+	echo $MONGODB1
+	echo $MONGODB2
+	echo $MONGODB3
+
+	echo 'Conecting mongo1 with mongo2 and mongo3 in replica set'
+	#docker exec mongo1 mongo --eval 'rs.initiate(); cfg = rs.conf(); cfg.members[0].host = "$MONGODB1:27017"; rs.reconfig(cfg); rs.add("$MONGODB2:27017"); rs.add("$MONGODB3:27017"); rs.status();'
+	docker exec mongo1 mongo --eval "rs.initiate();"
+	sleep 5
+	docker exec mongo1 mongo --eval "cfg = rs.conf(); cfg.members[0].host = '$MONGODB1:27017'; rs.reconfig(cfg);"
+	sleep 5
+	docker exec mongo1 mongo --eval "rs.add('$MONGODB2:27017'); rs.add('$MONGODB3:27017');"
+	sleep 5
+	docker exec mongo1 mongo --eval "rs.status();"
+
+	#Dos instancias de mongo
+	echo "Running container with three nodes of mongo"
+	docker run -h "productosCuidadosDocker" -t -i -p 9000:9000 --rm -m $memoryLimit --memory-swap $swapMemory --cpu-shares=$cpuProportion --cpuset-cpus=$cpuCores --link mongo1:mongo1 --link mongo2:mongo2 --link mongo3:mongo3 play-scala-2-4:1.0-SNAPSHOT bin/
+
+fi
